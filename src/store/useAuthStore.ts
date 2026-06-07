ec2b00bafaa80fd7d95f87429a456bc2d42ce937
DimaAnaMaria <dimaanamaria04@gmail.com>
2026-06-07 21:44:38 +0300

    Clear profile on login/logout

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -10,29 +10,32 @@ interface AuthState {
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
-  user:  null,
+  user: null,
   token: null,
 
   login: (user, token) => {
     localStorage.setItem('auth_token', token);
     localStorage.setItem('auth_user', JSON.stringify(user));
+    // Sterge profilul activ al utilizatorului anterior
+    localStorage.removeItem('active_profile');
     set({ user, token });
   },
 
   logout: () => {
     localStorage.removeItem('auth_token');
     localStorage.removeItem('auth_user');
+    localStorage.removeItem('active_profile');
     set({ user: null, token: null });
   },
 
   initialize: () => {
-    const token   = localStorage.getItem('auth_token');
+    const token = localStorage.getItem('auth_token');
     const userStr = localStorage.getItem('auth_user');
     if (token && userStr) {
       try {
         const user = JSON.parse(userStr) as User;
         set({ user, token });
-      } catch {}
+      } catch { }
     }
   },
 }));
